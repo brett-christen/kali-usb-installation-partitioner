a48b8bc6a4496f69425407921d9182cb29596d6b
brett <24708310+dahop@users.noreply.github.com>
2022-10-31 13:17:17 -0400

    kali-usb-installation-partitioner.sh
    Taken from the docs

---
+++ kali-usb-installation-partitioner.sh
@@ -0,0 +1,27 @@
+sgdisk --zap-all /dev/sdb
+sgdisk --new=1:0:+4096M /dev/sdb
+sgdisk --new=2:0:+2M /dev/sdb
+sgdisk --new=3:0:+128M /dev/sdb
+sgdisk --new=4:0:+8192M /dev/sdb
+sgdisk --new=5:0:0 /dev/sdb
+sgdisk --typecode=1:8301 --typecode=2:ef02 --typecode=3:ef00 --typecode=4:8200 --typecode=5:8300 /dev/sdb
+sgdisk --change-name=1:/boot --change-name=2:GRUB --change-name=3:EFI-SP --change-name=4:swap --change-name=5:rootfs /dev/sdb
+sgdisk --hybrid 1:2:3 /dev/sdb
+sgdisk --print /dev/sdb
+
+cryptsetup luksFormat --type=luks1 /dev/sda1
+cryptsetup luksFormat /dev/sda4
+cryptsetup luksFormat /dev/sda5
+
+cryptsetup open /dev/sda1 LUKS_BOOT
+cryptsetup open /dev/sda4 LUKS_SWAP
+cryptsetup open /dev/sda5 LUKS_ROOT
+
+mkfs.ext4 -L boot /dev/mapper/LUKS_BOOT
+mkfs.vfat -F 16 -n EFI-SP /dev/sda3
+mkswap -L swap /dev/mapper/LUKS_SWAP
+mkfs.btrfs -L root /dev/mapper/LUKS_ROOT
+
+cryptsetup close LUKS_BOOT
+cryptsetup close LUKS_SWAP
+cryptsetup close LUKS_ROOT
